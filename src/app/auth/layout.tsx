c807e0433000a182d670b70bf9af8b5f313d5f3a
Asad_Saeed <asadsaeed.tech@gmail.com>
2026-05-11 19:57:23 +0500

    Refactor code structure for improved readability and maintainability

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,16 +1,16 @@
-import { currentUser } from '@clerk/nextjs'
-import Image from 'next/image'
-import { redirect } from 'next/navigation'
-import React from 'react'
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import { redirect } from "next/navigation";
+import React from "react";
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 const Layout = async ({ children }: Props) => {
-  const user = await currentUser()
+  const user = await currentUser();
 
-  if (user) redirect('/')
+  if (user) redirect("/");
 
   return (
     <div className="h-screen flex w-full justify-center">
@@ -20,8 +20,8 @@ const Layout = async ({ children }: Props) => {
           alt="LOGO"
           sizes="100vw"
           style={{
-            width: '20%',
-            height: 'auto',
+            width: "20%",
+            height: "auto",
           }}
           width={0}
           height={0}
@@ -30,10 +30,10 @@ const Layout = async ({ children }: Props) => {
       </div>
       <div className="hidden lg:flex flex-1 w-full max-h-full max-w-4000px overflow-hidden relative bg-white  flex-col pt-10 pl-24 gap-3">
         <h2 className="text-grayblack md:text-4xl font-bold">
-          Hi, I’m your AI powered sales assistant, Corinna!
+          Hi, I’m your AI powered sales assistant, Aesther!
         </h2>
         <p className="text-grayblack md:text-sm mb-10">
-          Corinna is capable of capturing lead information without a form...{' '}
+          Aesther is capable of capturing lead information without a form...{" "}
           <br />
           something never done before 😉
         </p>
@@ -48,7 +48,7 @@ const Layout = async ({ children }: Props) => {
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
